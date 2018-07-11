18d8bad728ccc4af1f88b8d2747af22000b5eb05
alexfilimon <as_filimon@mail.ru>
2018-07-11 21:46:41 +0300

    create new models, in progress schemecontroller

---
+++ JBBApp/Scheme/Controller/InfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InfoTableViewCell.swift
+//  JBBApp
+//
+//  Created by Александр Филимонов on 11/07/2018.
+//  Copyright © 2018 Alex Filimonov. All rights reserved.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ JBBApp/Scheme/Controller/SchemeControllerNew.swift
@@ -0,0 +1,9 @@
+//
+//  SchemeControllerNew.swift
+//  JBBApp
+//
+//  Created by Александр Филимонов on 11/07/2018.
+//  Copyright © 2018 Alex Filimonov. All rights reserved.
+//
+
+import Foundation
